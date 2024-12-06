56998326912d2b05da54886df17d8070d970c68f
Betinsk <gmdust@hotmail.com>
2024-12-06 17:53:07 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import './App.css';
-<<<<<<< HEAD
-import React, { useState, useEffect, useMemo, useRef, useContext,  } from 'react';
-=======
-import React, { useState, useEffect, useMemo, useRef, useContext, createContext } from 'react';
+import {  useContext,  } from 'react';
+import React, { useState, useEffect, useMemo, useRef,  createContext } from 'react';
 
-
-
-
-
->>>>>>> ddea69fa7c48f636863aaf5ef77f2f5abe823be7
-
-
-<<<<<<< HEAD
+const globalState = {
+  title: 'Ola',
+  counter:0,
+  body: "Ola meu povo"
+}
 const GlobalContext = React.createContext()
 
 const Div = ({children}) => {
+  
   return(
     <H1>Oi</H1>
   )
@@ -22,26 +18,23 @@ const Div = ({children}) => {
 
 const H1 = ({}) => {
   const theContext = useContext(GlobalContext)
-  console.log(theContext)
+  const { contextState: {body, counter}, contextState, setContextState}  = theContext
   return(
-    <h1 className='App'>{theContext.title}</h1>
+    <>
+    <h1 className='App'>{contextState.title} {counter}</h1>
+    <p onClick={() => setContextState(s => ({...s, counter: s.counter + 1}))} className='App'>{contextState.body}</p>
+    </>
+
   )
 }
 
 function App() {
 
-  return (
-      <GlobalContext.Provider value={globalState}>
-=======
-
-function App() {
-
-    const [contextState, setContextState] = useState(globalState)
+  const [contextState, setContextState ] = useState(globalState)
   
   return (
-     
+      
       <GlobalContext.Provider value={{contextState, setContextState}}>
->>>>>>> ddea69fa7c48f636863aaf5ef77f2f5abe823be7
         <Div />
       </GlobalContext.Provider>
    
